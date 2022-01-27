1f310c16ec67cb3bc72da750b927c15b1530ef99
ellyysium <92124885+ellyysium@users.noreply.github.com>
2022-01-27 15:53:22 -0300

    Add files via upload

---
+++ syscallsstubs.asm
@@ -0,0 +1,131 @@
+.code
+
+EXTERN SW2_GetSyscallNumber: PROC
+
+NtWaitForSingleObject PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0A516DFD8h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtWaitForSingleObject ENDP
+
+NtClose PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 034DFDD53h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtClose ENDP
+
+NtFreeVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 08F1F7668h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtFreeVirtualMemory ENDP
+
+NtAllocateVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 049803B6Bh        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtAllocateVirtualMemory ENDP
+
+NtWriteVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 00190131Bh        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtWriteVirtualMemory ENDP
+
+NtCreateThreadEx PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0043FD765h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtCreateThreadEx ENDP
+
+NtOpenProcess PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0D2ACC904h        ; Load function hash into ECX.
+	call SW2_GetSyscallNumber  ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp +8]          ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	syscall                    ; Invoke system call.
+	ret
+NtOpenProcess ENDP
+
+end
